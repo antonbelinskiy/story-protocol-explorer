2692a252b6b55521bc1228981e4fd937057f2a9c
Anton Belinskyi <jjusaaw@gmail.com>
2024-10-20 12:32:40 +0300

    changed grafana guide

--- src/pages/InstallingManual.jsx
+++ src/pages/InstallingManual.jsx
@@ -314,12 +314,13 @@ sudo systemctl status prometheusd.service`}
 
                 <h4>Install Grafana</h4>
                 <CodeBlock
-                    codeText={`sudo apt-get install -y apt-transport-https
-sudo apt-get install -y software-properties-common wget
-sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
-echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana-enterprise`}
+                    codeText={`sudo apt install -y apt-transport-https software-properties-common wget
+            sudo mkdir -p /etc/apt/keyrings/
+            wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+            echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+            sudo apt update
+            sudo apt install grafana
+`}
                 />
 
                 <h4>
